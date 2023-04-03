72ad6fc06a173dc27e4d531ba4005f41cf931118
Zé Florentino <114265710+zeflorentino@users.noreply.github.com>
2023-04-03 19:48:46 -0300

    Create consulta.py

---
+++ consulta.py
@@ -0,0 +1,55 @@
+import pandas as pd
+
+def ponto_para_virgula(numero):
+  numero = float(numero)
+  numero = round(numero, 3)
+  numero = str(numero)
+  numero = numero.replace('.0', '')
+  numero = numero.replace('.', ',')
+  return numero
+
+def puxa_historico(link):
+  tabela = pd.read_csv(link)
+  tabela = tabela.query('Commodity == "Corn" and MarketYear	== "2022/23" and ReportTitle == "World Corn Supply and Use"')  
+  tabela = tabela.loc[:, ['Commodity', 'Region', 'Attribute', 'Value']] 
+  mensagem_final = f""
+  paises = ['World', 'United States', 'Brazil', 'Argentina', 'Ukraine']
+  projecoes = ['Production', 'Domestic Total', 'Exports', 'Ending Stocks']
+  
+  for pais in paises:
+    tabela_nova = tabela.query('Region == @pais')  
+    for projecao in projecoes:
+      tabela_projecao = tabela_nova.query('Attribute == @projecao')
+      tabela_projecao = tabela_projecao.reset_index(drop=True)
+      projecoes_usda = {'Production' : 'produção', 'Domestic Total' : 'demanda', 'Exports' : 'exportação', 'Ending Stocks' : 'estoques finais'}
+
+      paises = {'World': 'mundial', 'United States' : 'nos EUA', 'Brazil' : 'no Brasil', 'Argentina' : 'na Argentina', 'Ukraine' : 'na Ucrânia'}
+        
+      pais_corrigir = pais
+      for chave, valor in paises.items():
+        pais_corrigir = pais_corrigir.replace(chave, valor)    
+
+      projecao_corrigir = projecao     
+      for chave, valor in projecoes_usda.items():
+        projecao_corrigir = projecao_corrigir.replace(chave, valor)
+      
+      valor = tabela_projecao['Value'].iloc[0]
+
+      if not tabela_projecao['Value'].empty:
+        
+        if valor >= 1000.0:
+          valor = valor / 1000
+          valor = ponto_para_virgula(valor)
+          complemento = "bilhão"
+        elif valor > 2:
+          complemento = "milhões"
+          valor = ponto_para_virgula(valor)
+        else:
+          complemento = "milhão"
+          valor = ponto_para_virgula(valor)
+        
+      mensagem_final += f'Milho: USDA estimava {projecao_corrigir} {pais_corrigir} na safra 2022/23 em {valor} {complemento} de toneladas.\n'
+        
+      
+  return mensagem_final
+      
