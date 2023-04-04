6616d4ce82d22ffdae356728a9fcc00391edc3ae
Zé Florentino <114265710+zeflorentino@users.noreply.github.com>
2023-04-04 19:27:24 -0300

    Update app.py

--- app.py
+++ app.py
@@ -13,45 +13,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def menu():
-  texto_final = f"""<div style="text-align: center;">
-  <h1 style="font-family: Verdana;">Relatório do USDA</h1>
-  <button style="font-family: Verdana; font-size: 16px; color: white; background-color: black; padding: 10px; border: none; border-radius: 5px; margin-bottom: 5px;">
-    <a href="/milho-atual" style="text-decoration: none; color: inherit;">Milho</a>
-  </button>
-  <br>
-  <button style="font-family: Verdana; font-size: 16px; color: white; background-color: black; padding: 10px; border: none; border-radius: 5px; margin-bottom: 5px;">
-    <a href="/milho-anterior" style="text-decoration: none; color: inherit;">Milho Antigo</a>
-  </button>
-  <br>
-  <button style="font-family: Verdana; font-size: 16px; color: white; background-color: black; padding: 10px; border: none; border-radius: 5px; margin-bottom: 5px;">
-    <a href="/soja-atual" style="text-decoration: none; color: inherit;">Soja</a>
-  </button>
-  <br>
-  <button style="font-family: Verdana; font-size: 16px; color: white; background-color: black; padding: 10px; border: none; border-radius: 5px; margin-bottom: 5px;">
-    <a href="/soja-anterior" style="text-decoration: none; color: inherit;">Soja Antigo</a>
-  </button>
-  <br>
-  <button style="font-family: Verdana; font-size: 16px; color: white; background-color: black; padding: 10px; border: none; border-radius: 5px; margin-bottom: 5px;">
-    <a href="/trigo-atual" style="text-decoration: none; color: inherit;">Trigo</a>
-  </button>
-  <br>
-  <button style="font-family: Verdana; font-size: 16px; color: white; background-color: black; padding: 10px; border: none; border-radius: 5px; margin-bottom: 5px;">
-    <a href="/trigo-anterior" style="text-decoration: none; color: inherit;">Trigo Antigo</a>
-  </button>
-  <br>
-  <button style="font-family: Verdana; font-size: 16px; color: white; background-color: black; padding: 10px; border: none; border-radius: 5px; margin-bottom: 5px;">
-    <a href="/algodao-atual" style="text-decoration: none; color: inherit;">Algodão</a>
-  </button>
-  <br>
-  <button style="font-family: Verdana; font-size: 16px; color: white; background-color: black; padding: 10px; border: none; border-radius: 5px; margin-bottom: 5px;">
-    <a href="/algodao-anterior" style="text-decoration: none; color: inherit;">Algodão Antigo</a>
-  </button>
-  <br>
-  <button style="font-family: Verdana; font-size: 16px; color: white; background-color: black; padding: 10px; border: none; border-radius: 5px;">
-    <a href="/consulta" style="text-decoration: none; color: inherit;">Consulta Relatórios</a>
-  </button>
-</div>
-"""
+  menu =  render_template('menu.html')
+  texto_final = f"<div style="text-align: center;"><h1 style="font-family: Verdana;">Relatório do USDA</h1></div><br><br}{menu}"
   return texto_final
 
 @app.route("/milho-atual")
@@ -59,9 +22,9 @@ def milho_atual():
   mes = datetime.now().strftime('%m')
   ano = datetime.now().strftime('%Y')
   texto_meio = funcoes.escreve(mes, ano, "milho")
-  texto_final = f"""<font face = "Tahoma" size = "6"><strong>Relatório deste mês.</strong></font><br><br>
+  texto_final = f"""<font face = "Tahoma" size = "5"><strong>Relatório deste mês.</strong></font><br><br>
         {texto_meio}
-        <center><a href="/milho-anterior"><font face = "Tahoma" size = "5"><strong>Relatório do mês passado.</strong></font></a><br><a href="/"><font face = "Tahoma" size = "4"><strong>Retorne ao menu</strong></font></a></center>"""
+        <center><a href="/milho-anterior"><font face = "Tahoma" size = "4"><strong>Relatório do mês passado.</strong></font></a><br><a href="/"><font face = "Tahoma" size = "3"><strong>Retorne ao menu</strong></font></a></center>"""
   return texto_final
   
 @app.route("/milho-anterior")
@@ -72,9 +35,9 @@ def milho_anterior():
   mes = data_anterior.strftime('%m')
   ano = data_anterior.strftime('%Y')
   texto_meio = funcoes.escreve(mes, ano, "milho")
-  texto_final = f"""<font face = "Tahoma" size = "6"><strong>Relatório do mês passado.</strong></font><br><br>
+  texto_final = f"""<font face = "Tahoma" size = "5"><strong>Relatório do mês passado.</strong></font><br><br>
         {texto_meio}
-        <center><a href="/milho-atual"><font face = "Tahoma" size = "5"><strong>Relatório deste mês.</strong></font></a><br><a href="/"><font face = "Tahoma" size = "4"><strong>Retorne ao menu</strong></font></a></center>"""
+        <center><a href="/milho-atual"><font face = "Tahoma" size = "4"><strong>Relatório deste mês.</strong></font></a><br><a href="/"><font face = "Tahoma" size = "3"><strong>Retorne ao menu</strong></font></a></center>"""
   return texto_final
 
 @app.route("/soja-atual")
@@ -82,9 +45,9 @@ def soja_atual():
   mes = datetime.now().strftime('%m')
   ano = datetime.now().strftime('%Y')
   texto_meio = funcoes.escreve(mes, ano, "soja")
-  texto_final = f"""<font face = "Tahoma" size = "6"><strong>Relatório deste mês.</strong></font><br><br>
+  texto_final = f"""<font face = "Tahoma" size = "5"><strong>Relatório deste mês.</strong></font><br><br>
         {texto_meio}
-        <center><a href="/soja-anterior"><font face = "Tahoma" size = "5"><strong>Relatório do mês passado.</strong></font></a><br><a href="/"><font face = "Tahoma" size = "4"><strong>Retorne ao menu</strong></font></a></center>"""
+        <center><a href="/soja-anterior"><font face = "Tahoma" size = "4"><strong>Relatório do mês passado.</strong></font></a><br><a href="/"><font face = "Tahoma" size = "3"><strong>Retorne ao menu</strong></font></a></center>"""
   return texto_final
   
 @app.route("/soja-anterior")
@@ -95,9 +58,9 @@ def soja_anterior():
   mes = data_anterior.strftime('%m')
   ano = data_anterior.strftime('%Y')
   texto_meio = funcoes.escreve(mes, ano, "soja")
-  texto_final = f"""<font face = "Tahoma" size = "6"><strong>Relatório do mês passado.</strong></font><br><br>
+  texto_final = f"""<font face = "Tahoma" size = "5"><strong>Relatório do mês passado.</strong></font><br><br>
         {texto_meio}
-        <center><a href="/soja-atual"><font face = "Tahoma" size = "5"><strong>Relatório deste mês.</strong></font></a><br><a href="/"><font face = "Tahoma" size = "4"><strong>Retorne ao menu</strong></font></a></center>"""
+        <center><a href="/soja-atual"><font face = "Tahoma" size = "4"><strong>Relatório deste mês.</strong></font></a><br><a href="/"><font face = "Tahoma" size = "3"><strong>Retorne ao menu</strong></font></a></center>"""
   return texto_final
 
 @app.route("/trigo-atual")
@@ -105,9 +68,9 @@ def trigo_atual():
   mes = datetime.now().strftime('%m')
   ano = datetime.now().strftime('%Y')
   texto_meio = funcoes.escreve(mes, ano, "trigo")
-  texto_final = f"""<font face = "Tahoma" size = "6"><strong>Relatório deste mês.</strong></font><br><br>
+  texto_final = f"""<font face = "Tahoma" size = "5"><strong>Relatório deste mês.</strong></font><br><br>
         {texto_meio}
-        <center><a href="/trigo-anterior"><font face = "Tahoma" size = "5"><strong>Relatório do mês passado.</strong></font></a><br><a href="/"><font face = "Tahoma" size = "4"><strong>Retorne ao menu</strong></font></a></center>"""
+        <center><a href="/trigo-anterior"><font face = "Tahoma" size = "4"><strong>Relatório do mês passado.</strong></font></a><br><a href="/"><font face = "Tahoma" size = "3"><strong>Retorne ao menu</strong></font></a></center>"""
   return texto_final
   
 @app.route("/trigo-anterior")
@@ -118,9 +81,9 @@ def trigo_anterior():
   mes = data_anterior.strftime('%m')
   ano = data_anterior.strftime('%Y')
   texto_meio = funcoes.escreve(mes, ano, "trigo")
-  texto_final = f"""<font face = "Tahoma" size = "6"><strong>Relatório do mês passado.</strong></font><br><br>
+  texto_final = f"""<font face = "Tahoma" size = "5"><strong>Relatório do mês passado.</strong></font><br><br>
         {texto_meio}
-        <center><a href="/trigo-atual"><font face = "Tahoma" size = "5"><strong>Relatório deste mês.</strong></font></a><br><a href="/"><font face = "Tahoma" size = "4"><strong>Retorne ao menu</strong></font></a></center>"""
+        <center><a href="/trigo-atual"><font face = "Tahoma" size = "4"><strong>Relatório deste mês.</strong></font></a><br><a href="/"><font face = "Tahoma" size = "3"><strong>Retorne ao menu</strong></font></a></center>"""
   return texto_final
 
 @app.route("/algodao-atual")
@@ -128,9 +91,9 @@ def algodao_atual():
   mes = datetime.now().strftime('%m')
   ano = datetime.now().strftime('%Y')
   texto_meio = funcoes.escreve(mes, ano, "algodao")
-  texto_final = f"""<font face = "Tahoma" size = "6"><strong>Relatório deste mês.</strong></font><br><br>
+  texto_final = f"""<font face = "Tahoma" size = "5"><strong>Relatório deste mês.</strong></font><br><br>
         {texto_meio}
-        <center><a href="/trigo-anterior"><font face = "Tahoma" size = "5"><strong>Relatório do mês passado.</strong></font></a><br><a href="/"><font face = "Tahoma" size = "4"><strong>Retorne ao menu</strong></font></a></center>"""
+        <center><a href="/trigo-anterior"><font face = "Tahoma" size = "4"><strong>Relatório do mês passado.</strong></font></a><br><a href="/"><font face = "Tahoma" size = "3"><strong>Retorne ao menu</strong></font></a></center>"""
   return texto_final
   
 @app.route("/algodao-anterior")
@@ -141,9 +104,9 @@ def algodao_anterior():
   mes = data_anterior.strftime('%m')
   ano = data_anterior.strftime('%Y')
   texto_meio = funcoes.escreve(mes, ano, "algodao")
-  texto_final = f"""<font face = "Tahoma" size = "6"><strong>Relatório do mês passado.</strong></font><br><br>
+  texto_final = f"""<font face = "Tahoma" size = "5"><strong>Relatório do mês passado.</strong></font><br><br>
         {texto_meio}
-        <center><a href="/trigo-atual"><font face = "Tahoma" size = "5"><strong>Relatório deste mês.</strong></font></a><br><a href="/"><font face = "Tahoma" size = "4"><strong>Retorne ao menu</strong></font></a></center>"""
+        <center><a href="/trigo-atual"><font face = "Tahoma" size = "4"><strong>Relatório deste mês.</strong></font></a><br><a href="/"><font face = "Tahoma" size = "3"><strong>Retorne ao menu</strong></font></a></center>"""
   return texto_final
   
 @app.route("/botdoze", methods=["POST"])
@@ -164,7 +127,7 @@ def resultado():
   safra = str(request.form['safra'])
   produto = str(request.form['produto'])
   resultado = funcoes.historico(link, produto, safra)
-  mostra = f"""<font face = "Tahoma" size = "6"><strong>Destaques do relatório de {mes}/{ano} sobre a safra {safra} de {produto}:</strong></font><br><br>{resultado}"""
+  mostra = f"""<font face = "Tahoma" size = "4"><strong>Destaques do relatório de {mes}/{ano} sobre a safra {safra} de {produto}:</strong></font><br><br>{resultado}"""
   return mostra
 
 if __name__ == '__main__':
