f59403fbc905e02e80858a5d03386ceae2f67827
Zé Florentino <114265710+zeflorentino@users.noreply.github.com>
2023-04-03 22:08:57 -0300

    Update app.py

--- app.py
+++ app.py
@@ -112,7 +112,7 @@ def resultado():
   safra = str(request.form['safra'])
   produto = str(request.form['produto'])
   resultado = funcoes.historico(link, produto, safra)
-  mostra = f"<font face = "Tahoma" size = "6"><strong>Veja os resultados!</strong></font><br><br>{resultado}"
+  mostra = f"<font face = "Tahoma" size = "6"><strong>Destaques do relatório de {mes}/{ano} sobre a safra {safra} de {produto}:</strong></font><br><br>{resultado}"
   return mostra
 
 if __name__ == '__main__':
